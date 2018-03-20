436e4e1945cb5436445db690c10e310a2ae03e50
Gyanendra Chaubey <gyanendra.chaubey@gmail.com>
2018-03-20 22:13:40 +0530

    Revert "Adding some bean based configuration and mixing of bean and java based configuration."
    This reverts commit 47c105c263590f6fb819d42ec154d79dd925f859.

--- src/main/java/com/gyan/springlearning/beanwiring/impl/CDPlayer.java
+++ src/main/java/com/gyan/springlearning/beanwiring/impl/CDPlayer.java
@@ -5,6 +5,7 @@ import com.gyan.springlearning.beanwiring.interfaces.MediaPlayer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@Component
 public class CDPlayer implements MediaPlayer {
     private CompactDisc cd;
 
--- src/main/java/com/gyan/springlearning/beanwiring/impl/SonuNigammMusic.java
+++ src/main/java/com/gyan/springlearning/beanwiring/impl/SonuNigammMusic.java
@@ -3,6 +3,7 @@ package com.gyan.springlearning.beanwiring.impl;
 import com.gyan.springlearning.beanwiring.interfaces.CompactDisc;
 import org.springframework.stereotype.Component;
 
+@Component
 public class SonuNigammMusic implements CompactDisc {
     public void play() {
         System.out.println("Bewafa Sanam");
